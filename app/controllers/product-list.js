4ffd08112407ee46852fe2d0928efdbea95b0ac0
Ankeet Maini <ankeet.maini@gmail.com>
2015-04-07 18:31:10 +0530

    added pagination + dist

--- app/controllers/product-list.js
+++ app/controllers/product-list.js
@@ -1,10 +1,29 @@
 angular.module('sportsStore')
-  .constant("productListActiveClass", "btn-primary")
-  .controller('ProductListController', function ($scope, $filter, productListActiveClass) {
+  .constant('productListActiveClass', 'btn-primary')
+  .constant('productListPageCount', 3)
+  .controller('ProductListController', function ($scope, $filter, productListActiveClass,
+    productListPageCount) {
     var selectedCategory = null;
 
+    // pagination state
+    $scope.selectedPage = 1;
+    $scope.pageSize = productListPageCount;
+
+    // called each time the menu buttons are clicked
     $scope.selectCategory = function selectCategory (newCategory) {
       selectedCategory = newCategory;
+
+      // set page number to 1
+      $scope.selectedPage = 1;
+    };
+
+    $scope.selectPage = function selectPage (newPage) {
+      $scope.selectedPage = newPage;
+    };
+
+    // to get the active page class
+    $scope.getPageClass = function (page) {
+      return $scope.selectedPage == page ? productListActiveClass : "";
     };
 
     $scope.categoryFilterFn = function categoryFilterFn (product) {
